a1761219d58a72c825796416e657384911f3fb42
brunocssouza <92397493+brunocssouza@users.noreply.github.com>
2024-11-02 11:35:30 -0300

    Update Corra_Fisico.sql

--- 01-11/Corra_Fisico.sql
+++ 01-11/Corra_Fisico.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE Personagem (
     Personagem_ID INTEGER PRIMARY KEY,
-    Personagem_Nome VARCHAR(30),
-    Etnia VARCHAR(30),
-    Genero VARCHAR(30),
+    Personagem_Nome VARCHAR(30) NOT NULL,
+    Etnia VARCHAR(30) NOT NULL,
+    Genero VARCHAR(30) NOT NULL,
     fk_Tratamento_Tratamento_ID INTEGER
 );
 
 CREATE TABLE Familia (
     Familia_ID INTEGER PRIMARY KEY,
-    Familia_Nome VARCHAR(30)
+    Familia_Nome VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Contexto_Sociocultural_Possui (
@@ -23,66 +23,12 @@ CREATE TABLE Contexto_Sociocultural_Possui (
 
 CREATE TABLE Tratamento (
     Tratamento_ID INTEGER PRIMARY KEY,
-    Tratamento_Nome VARCHAR(30),
-    Descricao VARCHAR(30)
-);
-
-CREATE TABLE MembroFamilia_Tem (
-    Estado_Psicologico VARCHAR(30),
-    fk_Personagem_Personagem_ID INTEGER,
-    fk_Familia_Familia_ID INTEGER
-);
- 
-ALTER TABLE Personagem ADD CONSTRAINT FK_Personagem_2
-    FOREIGN KEY (fk_Tratamento_Tratamento_ID)
-    REFERENCES Tratamento (Tratamento_ID)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE Contexto_Sociocultural_Possui ADD CONSTRAINT FK_Contexto_Sociocultural_Possui_1
-    FOREIGN KEY (fk_Familia_Familia_ID)
-    REFERENCES Familia (Familia_ID);
- 
-ALTER TABLE Contexto_Sociocultural_Possui ADD CONSTRAINT FK_Contexto_Sociocultural_Possui_2
-    FOREIGN KEY (fk_Personagem_Personagem_ID)
-    REFERENCES Personagem (Personagem_ID);
- 
-ALTER TABLE MembroFamilia_Tem ADD CONSTRAINT FK_MembroFamilia_Tem_1
-    FOREIGN KEY (fk_Personagem_Personagem_ID)
-    REFERENCES Personagem (Personagem_ID);
- 
-ALTER TABLE MembroFamilia_Tem ADD CONSTRAINT FK_MembroFamilia_Tem_2
-    FOREIGN KEY (fk_Familia_Familia_ID)
-    REFERENCES Familia (Familia_ID);/* Corra_Logico: */
-
-CREATE TABLE Personagem (
-    Personagem_ID INTEGER PRIMARY KEY,
-    Personagem_Nome VARCHAR(30),
-    Etnia VARCHAR(30),
-    Genero VARCHAR(30),
-    fk_Tratamento_Tratamento_ID INTEGER
-);
-
-CREATE TABLE Familia (
-    Familia_ID INTEGER PRIMARY KEY,
-    Familia_Nome VARCHAR(30)
-);
-
-CREATE TABLE Contexto_Sociocultural_Possui (
-    Origem VARCHAR(30),
-    Classe_Social VARCHAR(30),
-    Crenca VARCHAR(30),
-    fk_Familia_Familia_ID INTEGER,
-    fk_Personagem_Personagem_ID INTEGER
-);
-
-CREATE TABLE Tratamento (
-    Tratamento_ID INTEGER PRIMARY KEY,
-    Tratamento_Nome VARCHAR(30),
+    Tratamento_Nome VARCHAR(30) NOT NULL,
     Descricao VARCHAR(200)
 );
 
 CREATE TABLE MembroFamilia_Tem (
-    Estado_Psicologico VARCHAR(30),
+    Estado_Psicologico VARCHAR(30) NOT NULL,
     fk_Personagem_Personagem_ID INTEGER,
     fk_Familia_Familia_ID INTEGER
 );

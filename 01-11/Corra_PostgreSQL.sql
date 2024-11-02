e7b73b58206dd1429e0353a8cb04173d1386b7b8
brunocssouza <92397493+brunocssouza@users.noreply.github.com>
2024-11-02 11:55:56 -0300

    Add files via upload

--- 01-11/Corra_PostgreSQL.sql
+++ 01-11/Corra_PostgreSQL.sql
@@ -1,3 +1,4 @@
+/** Relação Tratamento **/
 INSERT INTO tratamento (tratamento_id, tratamento_nome, descricao) VALUES
 (1, 'Manipulacao', 'Atração de afro-americanos para a região.'),
 (2, 'Controle', 'Utilização de hipnose para controle do indivíduo em questão.'),
@@ -5,12 +6,14 @@ INSERT INTO tratamento (tratamento_id, tratamento_nome, descricao) VALUES
 (4, 'Coagulacao', 'Fase final do tratamento onde o controle completo sobre o sujeito é exercido.');
 SELECT * FROM tratamento
 
+/** Relação Familia **/
 INSERT INTO familia (familia_id, familia_nome) VALUES
 (1, 'Armitage'),
 (2, 'Washington'),
 (3, 'Dray');
 SELECT * FROM familia
 
+/** Relação Personagem **/
 INSERT INTO personagem (personagem_id, personagem_nome, etnia, genero, fk_tratamento_tratamento_id) VALUES
 (1, 'Missy Armitage', 'Branco', 'Feminino', NULL),
 (2, 'Rose Armitage', 'Branco', 'Feminino', NULL),
@@ -19,6 +22,7 @@ INSERT INTO personagem (personagem_id, personagem_nome, etnia, genero, fk_tratam
 (5, 'Chris Washington', 'Negro', 'Masculino', 2);
 SELECT * FROM personagem
 
+/** Relação Contexto_Sociocultural **/
 INSERT INTO contexto_sociocultural_possui (origem, classe_social, crenca, fk_familia_familia_id, fk_personagem_personagem_id) VALUES
 ('Privilegiada', 'Alta', NULL, 1, 1),
 ('Privilegiada', 'Alta', NULL, 1, 1),
@@ -27,11 +31,11 @@ INSERT INTO contexto_sociocultural_possui (origem, classe_social, crenca, fk_fam
 ('Bairro pobre', 'Media', 'Cristão', 2, 5);
 SELECT * FROM contexto_sociocultural_possui
 
-
-INSERT INTO membrofamilia_tem (estado_psicologico, fk_personagem_personagem_id, fk_familia_familia_id) VALUES
+/** Relação MembroFamilia **/
+INSERT INTO membrofamilia (estado_psicologico, fk_personagem_personagem_id, fk_familia_familia_id) VALUES
 ('Espectador', 5, 2),
 ('Controlador', 1, 1),
 ('Controlador', 2, 1),
 ('Controlador', 3, 1),
 ('Controlador', 4, 1);
-SELECT * FROM membrofamilia_tem
+SELECT * FROM membrofamilia
